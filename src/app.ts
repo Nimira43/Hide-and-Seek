4ae30aec0c0319c7331cb4520f70e93b2d8cddfc
Nick Rabone <n.rabone72@aol.com>
2024-06-30 13:41:46 +0100

    Creating first Game scene

---
+++ src/app.ts
@@ -0,0 +1,19 @@
+import { ArcRotateCamera, Engine, Scene, Vector3 } from '@babylonjs/core'
+
+class Game {
+    constructor() {
+        const canvas = document.createElement('canvas')
+        canvas.style.width = '100%'
+        canvas.style.height = '100%'
+        canvas.id = 'gameCanvas'
+        document.appendChild(canvas)
+        
+        const engine = new Engine(canvas, true)
+        const scene = new Scene(engine)
+        const camera: ArcRotateCamera = new ArcRotateCamera('camera', Math.PI, Math.PI, 1 Vector3.Zero())
+        
+        engine.runRenderLoop(() => {
+            scene.render()
+        })
+    }
+}
