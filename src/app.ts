b957f434d160a0156837601a38787dbcbbfee58d
Nick Rabone <n.rabone72@aol.com>
2024-06-30 13:42:19 +0100

    Correction

--- src/app.ts
+++ src/app.ts
@@ -10,7 +10,7 @@ class Game {
         
         const engine = new Engine(canvas, true)
         const scene = new Scene(engine)
-        const camera: ArcRotateCamera = new ArcRotateCamera('camera', Math.PI, Math.PI, 1 Vector3.Zero())
+        const camera: ArcRotateCamera = new ArcRotateCamera('camera', Math.PI, Math.PI, 1, Vector3.Zero())
         
         engine.runRenderLoop(() => {
             scene.render()
