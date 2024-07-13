7ab392d299759743a95fc7b8c3ee15575ac2d6a9
Nick Rabone <n.rabone72@aol.com>
2024-07-13 10:17:33 +0100

    Updated

--- src/states/start.ts
+++ src/states/start.ts
@@ -1,17 +1,18 @@
 import { ArcRotateCamera, Engine, Scene, Vector3 } from '@babylonjs/core'
-import { AdvancedDynamicTexture, Rectangle, Button } from '@babylonjs/gui'
+import { AdvancedDynamicTexture, Rectangle, Image, Button } from '@babylonjs/gui'
 import { Game, Status } from '../app'
 
 export async function start(this: Game, canvas: HTMLCanvasElement, engine: Engine, status: Status) {
     
     status._scene.detachControl()
     engine.displayLoadingUI()
-    let sceneToLoad = new Scene(engine)
 
+    let sceneToLoad = new Scene(engine)
     const guiMenu = AdvancedDynamicTexture.CreateFullscreenUI('ui', true, sceneToLoad)
+    
     const background = new Rectangle('background')
     background.color = '#9dc9b5'
-    background.background = '#ffa500'
+    background.background = '#9cd9b5'
     guiMenu.addControl(background)
 
     const camera: ArcRotateCamera = new ArcRotateCamera('camera', Math.PI, Math.PI, 1, Vector3.Zero(), sceneToLoad)
